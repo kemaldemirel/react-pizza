a14e41c410c65e3d8c7a188f7538eb6609c0f971
Кемал Аджимамбетов <demirelkd0696@gmail.com>
2021-10-31 16:16:58 +0300

    add axios & propTypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import { Route, Switch } from 'react-router';
 import { Header } from './components';
 import { Home, Cart, EmptyPage } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get('http://localhost:3000/db.json')
+      .then(({ data }) => setPizzas(data.pizzas))
+      .catch((error) => console.log(error.message));
+  }, []);
+
   const showName = (name) => {
     console.log(name);
   };
@@ -16,7 +26,7 @@ function App() {
         <div className="content">
           <Switch>
             <Route path="/" exact>
-              <Home showName={showName} />
+              <Home showName={showName} data={pizzas} />
             </Route>
             <Route path="/cart" exact>
               <Cart />
